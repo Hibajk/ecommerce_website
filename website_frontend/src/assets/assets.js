053ece3142ba98720e38e58a2e5ca11b93788ead
Hibajk <hibachenni22@gmail.com>
2024-10-20 21:22:35 +0200

    creating assets.js file

---
+++ website_frontend/src/assets/assets.js
@@ -0,0 +1,75 @@
+import airpods from './airpods_icon.png'
+import appletv from './appletv_icon.png'
+import bimage from './b_image.jpg'
+import ipadicon from './ipad_icon.png'
+import iphoneicon from './iphone_icon.png'
+import logo from './logo.png'
+import watchicon from './watch_icon.png'
+import searchicon from './search_icon.png'
+import cardicon from './card_icon.png'
+import airpods4 from './airpods4.jpg'
+import airpodsmax from './airpodsmax.png'
+import airpodspro2 from './airpodspro2.png'
+import hermesultra2 from './hermesultra2.png'
+import ipad from './ipad.png'
+import ipadair from './ipadair.png'
+import ipadmini from './ipadmini.png'
+import ipadpro from './ipadpro.png'
+import iphone14 from './iphone14.png'
+import iphone15 from './iphone15.png'
+import iphone16 from './iphone16.png'
+import iphone16pro from './iphone16pro.png'
+import series10 from './series10.png'
+import ultra2 from './ultra2.png'
+import watchSE from './watchSE.png'
+import appletvset from './appletv.png'
+import rating_stars from './rating_stars.png'
+import minus from './minus.png'
+import plus from './plus.png'
+import plusg from './plusg.png'
+import facebook from './facebook.png'
+import instagram from './instagram.png'
+import twitter from './twitter.png'
+import footerlogo from './footerlogo.png'
+import appstore from './apple.png'
+import playstore from './google.png'
+import cross_icon from './cross_icon.png'
+
+export const assets = {
+    airpods,
+    appletv,
+    bimage,
+    ipadicon,
+    iphoneicon,
+    logo,
+    watchicon,
+    searchicon,
+    cardicon,
+    airpods4,
+    airpodsmax,
+    airpodspro2,
+    hermesultra2,
+    ipad,
+    ipadair,
+    ipadmini,
+    ipadpro,
+    iphone14,
+    iphone15,
+    iphone16,
+    iphone16pro,
+    series10,
+    ultra2,
+    watchSE,
+    appletvset,
+    rating_stars,
+    minus,
+    plus,
+    plusg,
+    facebook,
+    twitter,
+    instagram,
+    footerlogo,
+    appstore,
+    playstore,
+    cross_icon
+}
