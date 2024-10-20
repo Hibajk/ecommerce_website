91a0a721b670300e7c87215303466e69cc10a0fe
Hibajk <hibachenni22@gmail.com>
2024-10-20 22:21:39 +0200

    navbar creation completed

--- website_frontend/src/components/Navbar/Navbar.jsx
+++ website_frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+
+  const [menu,setMenu] = useState("Home")
+
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className='logo'/>
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</li>
+        <li onClick={()=>setMenu("Mobile App")} className={menu==="Mobile App"?"active":""}>Mobile App</li>
+        <li onClick={()=>setMenu("Contact us")} className={menu==="Contact us"?"active":""}>Contact us</li>
+      </ul>
+      <div className="navbar-right">
+      <div className='navbar-search-icon'>
+        <img src={assets.searchicon} alt="" />
+      </div>
+        <div className="navbar-search-icon">
+          <img src={assets.cardicon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign in</button>
+      </div>
+    </div>
   )
 }
 
