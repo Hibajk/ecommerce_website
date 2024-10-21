9a28b6dbc1edc3f150ea4e5d5a11e42549c5aba3
Hibajk <hibachenni22@gmail.com>
2024-10-22 00:58:05 +0200

    creating header.jsx file

---
+++ website_frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>All your favourite apple products here</h2>
+            <p>Experience the latest innovation from Apple. Explore our collection of iPhones, iPads, and more. Get the best deals on Apple products and discover a world of limitless possibilities.</p>
+            <button>Explore more</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
