1a2913329505aa5f003a7e194a65e5e70e5df695
Hibajk <hibachenni22@gmail.com>
2024-10-26 00:17:01 +0200

    explore products section completed

--- website_frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ website_frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-import './ExploreMenu.css'
+import  './ExploreMenu.css'
+import { products_list } from '../../assets/assets'
 
-const ExploreMenu = () => {
+
+const ExploreMenu = ({category,setCategory}) => {
   return (
-    <div>ExploreMenu</div>
+    <div className='explore-products' id='explore-products'>
+        <h1>Discover Products</h1>
+        <p className='explore-products-text'>Unlock a World of Endless Possibilities with Apple's Cutting-Edge Technology. Elevate Your Productivity, Creativity, and Entertainment with Apple's Comprehensive Ecosystem</p>
+        <div className="explore-products-list">
+            {products_list.map((item, index)=>{
+                return (
+                    <div onClick={()=>setCategory(prev=>prev===item.product_name?"All":item.product_name)} key={index} className="explore-products-list-item">
+                        <img className={category===item.product_name?"active":""} src={item.product_image} alt="" />
+                        <p>{item.product_name}</p>
+                    </div>
+                )
+            })}
+        </div>
+        <hr />
+    </div>
   )
 }
 
